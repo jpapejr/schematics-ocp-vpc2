afd66022d7c0269afcf4e917d4566f29077c1637
jpapejr <jpapejr@icloud.com>
2020-05-27 11:17:04 -0400

    removed outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# cluster config file path
-output "cluster_config_file_path" {
-  value = data.ibm_container_cluster_config.cluster_config.config_file_path
-}
-
-output "cluster_info" {
-  value = data.ibm_container_cluster_config.name
-}
-
