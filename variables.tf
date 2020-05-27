014446222eea79394c333e350fc67fcff34fbfc6
John Pape <jtpape@us.ibm.com>
2020-05-27 17:37:19 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
+variable "softlayer_username" {
+  description = "Enter your IBM Infrastructure (SoftLayer) full username, you can get this using: https://control.bluemix.net/account/user/profile"
+}
+
+variable "softlayer_api_key" {
+  description = "Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile"
+}
+
+variable "ibmcloud_api_key" {
+  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
+}
+
+
 variable "flavor" {
   default = "bx2.2x8"
 }
